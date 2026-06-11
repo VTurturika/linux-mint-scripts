5107ab3a1cb3285c6797c0dc6e4f57e7b2cadcc5
Victor Turturika <v.turturika@gmail.com>
2026-06-11 12:12:32 +0300

    feat: Add echos and checking for a sudo in template

--- 00_template.sh
+++ 00_template.sh
@@ -2,5 +2,15 @@
 
 set -e 
 
+# Ensure the script is run with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Error: Please run this script with sudo."
+  exit 1
+fi
 
+echo "🚀 Starting ..."
+
+
+
+echo "🎉 All done!"
 
